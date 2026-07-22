7360750190a28d40a21e39c26a0c78c10f2c427e
pranab56 <pronabhalder53@gmail.com>
2026-07-22 14:09:17 +0600

    all ok

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
 
   images: {
     remotePatterns: [
@@ -30,9 +29,7 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  turbopack: {
-    root: process.cwd(),
-  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
+
